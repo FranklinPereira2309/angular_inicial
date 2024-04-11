7d59ad86212e1c96884a1450d0c2de47a554dbfa
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-04-11 20:57:48 -0300

    Criando Property Binding

--- projeto-modulo2/src/app/app.component.ts
+++ projeto-modulo2/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-modulo2';
+  isDesabled = true;
+  textInput = 'text'
+
+  enableInput() {
+    this.isDesabled = false;
+  }
+  disabledInput() {
+    this.isDesabled = true;
+  }
+
+  toggleInput() {
+    this.textInput;
+  }
+
+  inputText() {
+    this.textInput = 'text';
+  }
+
+  inputPassword() {
+    this.textInput = 'password';
+  }
 }
