1023b8cdbde41d13c11d983e051f7b5ab30a5778
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-04-22 20:50:34 -0300

    boolean attribute

--- projeto-inicial/src/app/card-button/card-button.component.ts
+++ projeto-inicial/src/app/card-button/card-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
@@ -6,6 +6,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
+  @Input({transform: booleanAttribute}) buttonDisabled: boolean = false;
   @Output() buttonClickEmiter = new EventEmitter<string>();
 
   stringTeste: string = 'Fala man!';
--- projeto-inicial/src/app/card/card.component.ts
+++ projeto-inicial/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, numberAttribute } from '@angular/core';
 
 // function handlePlanType(value: string) {
 //   console.log('handlePlanType', value);
@@ -12,8 +12,10 @@ import { Component, Input } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  
   @Input('planText') textPlan: string = '';
   @Input('priceValue') valuePrice: number = 0;
+  //@Input({required:true, alias: 'testeTeste', transform: numberAttribute}) planPrice2: number = 0;
   //@Input({alias: 'planText1', transform:(value:string) => value.toUpperCase()}) planType1 : string = '';
 
   buttonClicked(valueEmitted: string) {
