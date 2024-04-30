fdd195ab08b2555c7a7c761557e3c1876d596316
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-04-30 10:31:52 -0300

    classes dinamicas

--- ngFor/src/app/app.component.ts
+++ ngFor/src/app/app.component.ts
@@ -6,10 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  personSelectedIndex: number | undefined;
   listPeople = [
     {name: 'Felipe Freitas', age: 26}, 
     {name: 'Fulano da Silva', age: 34}, 
     {name:'Jorginho Carvalho', age: 55}, 
     {name:'Joãozinho da Silva', age: 18}
   ];
+  selectPerson(index: number) {
+    this.personSelectedIndex = index;
+    
+  }
 }
