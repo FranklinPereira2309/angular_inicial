b68cc0e31298fb7bcf854a92541414eb97808863
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-05-06 16:23:48 -0300

    várias formas de se alterar dinamicamente

--- ngStyle/src/app/app.component.ts
+++ ngStyle/src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ngStyle';
+  tamanhoTexto: number = 15;
+  corTexto: 'white' | 'orange' = 'white' ;  
+  textoExibido: 'Branco' | 'Laranja' = 'Laranja'
+  //estilosPersonalizados: string = 'font-size: 15px; color: white'
+  estiloObj = {
+    'font-size': this.tamanhoTexto+'px',
+    'color': this.corTexto
+  }
+
+  incrementarFonte() {
+    this.tamanhoTexto += 5;
+    //this.estilosPersonalizados = `font-size: ${this.tamanhoTexto}px; color: ${this.corTexto}`
+    this.estiloObj = {
+      'font-size':this.tamanhoTexto+'px',
+      'color': this.corTexto
+    }
+  }
+  
+  trocarCorTexto() {
+    if(this.corTexto === 'white') {
+      this.corTexto = 'orange';
+      this.textoExibido = 'Branco';
+    }else {
+      this.corTexto = 'white';
+      this.textoExibido = 'Laranja';
+    }
+    //this.estilosPersonalizados = `font-size: ${this.tamanhoTexto}px; color: ${this.corTexto}`
+    this.estiloObj = {
+      'font-size':this.tamanhoTexto+'px',
+      'color': this.corTexto
+    }
+  }
 }
