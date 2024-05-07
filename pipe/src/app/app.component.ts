8cd2c3756c3ac2361e01b69feb9ca256db4c2992
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-05-07 10:48:09 -0300

    criação do pipe

--- pipe/src/app/app.component.ts
+++ pipe/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pipe';
+  text: string = 'Franklin';
+
+  obj1 = {
+    nome: 'Franklin',
+    status: 1
+  }
+  obj2 = {
+    nome: 'Marcos',
+    status: 2
+  }
+  obj3 = {
+    nome: 'Felipe',
+    status: 3
+  }
+
+  // getStyle(status: number) {
+  //   console.log('getStyle');
+    
+  //   return {
+  //     'active': status === 1,
+  //     'partial': status === 2,
+  //     'bloked': status === 3
+  //   }
+  // }
 }
