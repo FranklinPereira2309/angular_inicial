26554de12f99f74d63572a5c3c314749e03e6c07
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-05-21 21:57:07 -0300

    criando filtro por nome e status

--- users-filter-project/src/app/app.component.ts
+++ users-filter-project/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from '../interfaces/user/user.interface';
 import {UsersList} from '../data/users-list'
+import { IFilterOptions } from '../interfaces/filter-options-interface';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,15 @@ import {UsersList} from '../data/users-list'
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  userList: IUser[] = []
+  userList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
   
   ngOnInit(){
     setTimeout(()=> {
       this.userList = UsersList;
+      this.usersListFiltered = this.userList;
     }, 10);
   }
 
@@ -22,4 +25,44 @@ export class AppComponent implements OnInit {
     this.showUserDetails = true;
     this.userSelected = user;
   }
+
+  onFilter(filterOptions: IFilterOptions) {
+    
+    this.usersListFiltered = this.filtedUsersList(filterOptions, this.userList);
+    
+    
+  }
+
+  filtedUsersList(filterOptions: IFilterOptions, userList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+      filteredList = this.filtedUsersListByName(filterOptions.name, userList);
+      filteredList = this.filtedUsersListByStatus(filterOptions.status, filteredList);
+      
+    return filteredList;
+  }
+  filtedUsersListByStatus(status: boolean | undefined, filteredList: IUser[]): IUser[] {
+    const STATUS_NOT_SELECTED = status === undefined;
+    
+    if(STATUS_NOT_SELECTED) {
+      return filteredList;
+    }
+
+    const filtredStatusList = filteredList.filter((user) => user.ativo === status);
+
+    return filtredStatusList;
+  }
+
+  filtedUsersListByName(name: string | undefined, userList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined;
+
+    if(NAME_NOT_TYPPED) {
+      return userList;
+    }
+
+    const filteredList = userList.filter((user) => user.nome.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
+
+    return filteredList;
+  }
+
+ 
 }
