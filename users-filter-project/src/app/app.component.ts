45d77bf42ade685bcbff2b419fc19a0213faa99a
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-05-22 20:10:35 -0300

    criando o filtro por data

--- users-filter-project/src/app/app.component.ts
+++ users-filter-project/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from '../interfaces/user/user.interface';
 import {UsersList} from '../data/users-list'
 import { IFilterOptions } from '../interfaces/filter-options-interface';
+import { isWithinInterval } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -37,17 +38,33 @@ export class AppComponent implements OnInit {
     let filteredList: IUser[] = [];
       filteredList = this.filtedUsersListByName(filterOptions.name, userList);
       filteredList = this.filtedUsersListByStatus(filterOptions.status, filteredList);
-      
+      filteredList = this.filtedUsersListByDate(filterOptions.startDate, filterOptions.endDate, filteredList)
     return filteredList;
   }
-  filtedUsersListByStatus(status: boolean | undefined, filteredList: IUser[]): IUser[] {
+  filtedUsersListByDate(startDate: Date | undefined, endDate: Date | undefined, userList: IUser[]): IUser[] {
+    const DATES_NOT_SELECTED = startDate === undefined || endDate === undefined;
+
+    if(DATES_NOT_SELECTED) {
+      return userList;
+    }
+
+    const filtredDateList = userList.filter((user) => isWithinInterval(new Date(user.dataCadastro), {
+      start: startDate,
+      end: endDate
+    }));
+
+    return filtredDateList;
+
+
+  }
+  filtedUsersListByStatus(status: boolean | undefined, userList: IUser[]): IUser[] {
     const STATUS_NOT_SELECTED = status === undefined;
     
     if(STATUS_NOT_SELECTED) {
-      return filteredList;
+      return userList;
     }
 
-    const filtredStatusList = filteredList.filter((user) => user.ativo === status);
+    const filtredStatusList = userList.filter((user) => user.ativo === status);
 
     return filtredStatusList;
   }
